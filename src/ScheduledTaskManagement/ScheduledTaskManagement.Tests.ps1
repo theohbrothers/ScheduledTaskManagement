143973691db64ece138bec27cb206023ab954883
Joel Timothy Oh <joeltimothyoh@live.com>
2024-05-09 10:43:52 +0000

    Test (integration): Fix error handling for tests to correctly fail if non-terminating errors are returned

--- src/ScheduledTaskManagement/ScheduledTaskManagement.Tests.ps1
+++ src/ScheduledTaskManagement/ScheduledTaskManagement.Tests.ps1
@@ -7,35 +7,35 @@ Describe "ScheduledTaskManagement" -Tag 'Integration' {
         $script:stdout | Out-String -Stream | % { $_.Trim() } | ? { $_ } | Write-Host
     }
     It "Runs Setup-ScheduledTask -DefinitionFile" {
-        $script:stdout = Setup-ScheduledTask -DefinitionFile "definitions\scheduledtasks\tasks-1.ps1"
+        $script:stdout = Setup-ScheduledTask -DefinitionFile "definitions\scheduledtasks\tasks-1.ps1" -ErrorAction Stop
     }
     It "Runs Setup-ScheduledTask -DefinitionFile -AsJson" {
-        $script:stdout = Setup-ScheduledTask -DefinitionFile "definitions\scheduledtasks\tasks-1.json" -AsJson
+        $script:stdout = Setup-ScheduledTask -DefinitionFile "definitions\scheduledtasks\tasks-1.json" -AsJson -ErrorAction Stop
     }
     It "Runs Setup-ScheduledTask -DefinitionDirectory" {
-        $script:stdout = Setup-ScheduledTask -DefinitionDirectory "definitions\scheduledtasks\"
+        $script:stdout = Setup-ScheduledTask -DefinitionDirectory "definitions\scheduledtasks\" -ErrorAction Stop
     }
     It "Runs Setup-ScheduledTask -DefinitionDirectory -AsJson" {
-        $script:stdout = Setup-ScheduledTask -DefinitionDirectory "definitions\scheduledtasks\" -AsJson
+        $script:stdout = Setup-ScheduledTask -DefinitionDirectory "definitions\scheduledtasks\" -AsJson -ErrorAction Stop
     }
     It "Runs Setup-ScheduledTask -DefinitionObject" {
         $tasks = . ".\definitions\scheduledtasks\tasks-1.ps1"
-        $script:stdout = Setup-ScheduledTask -DefinitionObject $tasks
+        $script:stdout = Setup-ScheduledTask -DefinitionObject $tasks -ErrorAction Stop
     }
     It "Runs `$tasks | Setup-ScheduledTask from .ps1 definition file" {
         $tasks = . ".\definitions\scheduledtasks\tasks-1.ps1"
-        $script:stdout = $tasks | Setup-ScheduledTask
+        $script:stdout = $tasks | Setup-ScheduledTask -ErrorAction Stop
     }
     It "Runs `$tasks | Setup-ScheduledTask from .json definition file" {
         $tasks = Get-Content "definitions\scheduledtasks\tasks-1.json" | ConvertFrom-Json | % { $_ }
-        $script:stdout = $tasks | Setup-ScheduledTask
+        $script:stdout = $tasks | Setup-ScheduledTask -ErrorAction Stop
     }
     It "Runs `$tasks | Setup-ScheduledTask from directory containing .ps1 definition file(s)" {
         $tasks = Get-ChildItem "definitions\scheduledtasks\" -File | ? { $_.Extension -eq '.ps1' } | % { . $_.FullName }
-        $script:stdout = $tasks | Setup-ScheduledTask
+        $script:stdout = $tasks | Setup-ScheduledTask -ErrorAction Stop
     }
     It "Runs `$tasks | Setup-ScheduledTask from directory containing .json definition file(s)" {
         $tasks = Get-ChildItem "definitions\scheduledtasks\" -File | ? { $_.Extension -eq '.json' } | % { Get-Content $_.FullName | ConvertFrom-Json | % { $_ } }
-        $script:stdout = $tasks | Setup-ScheduledTask
+        $script:stdout = $tasks | Setup-ScheduledTask -ErrorAction Stop
     }
 }
